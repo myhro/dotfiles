959359c71e35d5132cb44d64ffd8fea9f50c3d75
Tiago Ilieve <tiago.myhro@gmail.com>
2017-09-12 17:27:15 +0200

    install.sh: only link tmux.conf under macOS

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 if [ -d .dotfiles ]; then
-    for f in bashrc gemrc screenrc tmux.conf vimrc; do
+    for f in bashrc gemrc screenrc vimrc; do
         ln -fs .dotfiles/$f ~/.$f
     done
     ln -fs ~/.bashrc ~/.profile
+    [ "$(uname -s)" = "Darwin" ] && ln -fs .dotfiles/tmux.conf .tmux.conf
     if [ "$1" = "vundle" ]; then
         mkdir -p ~/.vim/bundle/Vundle.vim
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
