ee6c5d917298956bfd2202f0330369807a964cdf
Tiago Ilieve <tiago.myhro@gmail.com>
2019-12-25 05:48:27 -0300

    Fix installation outside /home folder

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ -d .dotfiles ]; then
+set -eu
+
+DIR="$HOME/.dotfiles"
+
+if [ -d "$DIR" ]; then
     for f in bashrc gemrc screenrc vimrc; do
-        ln -fs .dotfiles/$f ~/.$f
+        ln -fs "$DIR/$f" "$HOME/.$f"
     done
     ln -fs ~/.bashrc ~/.profile
-    [ "$(uname -s)" = "Darwin" ] && ln -fs .dotfiles/tmux.conf .tmux.conf
-    if [ "$1" = "plug" ]; then
+    [ "$(uname -s)" = "Darwin" ] && ln -fs "$DIR/tmux.conf" "$HOME/.tmux.conf"
+    if [ "${1:-}" = "plug" ]; then
         mkdir -p ~/.vim/
         curl -o ~/.vim/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
